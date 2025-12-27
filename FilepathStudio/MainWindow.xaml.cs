638ccb6c59575c661c26e3e2251afbc41e1ce336
sashi <satoshi3104.nishi@gmail.com>
2025-12-27 13:30:30 +0900

    ダークテーマ

--- FilepathStudio/MainWindow.xaml.cs
+++ FilepathStudio/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FilepathStudio
 {
@@ -19,6 +11,9 @@ namespace FilepathStudio
         public MainWindow()
         {
             InitializeComponent();
+
+            Editor.Background = new SolidColorBrush(Color.FromRgb(32, 32, 32));
+            Editor.Foreground = Brushes.Gainsboro;
         }
     }
 }
